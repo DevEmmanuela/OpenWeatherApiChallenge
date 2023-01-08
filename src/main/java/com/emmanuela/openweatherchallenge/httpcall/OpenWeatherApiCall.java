44fd2a685a5ef4734b6074a3a13b0d23b64b6dc3
DevEmmanuela <ikeemmanuelachinwe@gmail.com>
2023-01-08 11:25:55 +0100

    feat/bck_version3 environmental variables

--- src/main/java/com/emmanuela/openweatherchallenge/httpcall/OpenWeatherApiCall.java
+++ src/main/java/com/emmanuela/openweatherchallenge/httpcall/OpenWeatherApiCall.java
@@ -20,7 +20,7 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class OpenWeatherApiCall {
 
-    @Value("${application.http.callout.api.openweather.baseurl}")
+    @Value("${application.http.callout.api.openweather.baseurl.apikeys}")
     private String baseUrl;
     private String modifiedWeatherUrl;
 
